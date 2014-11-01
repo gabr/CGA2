42c2d948d087a9521412405e4a4fd18cb74fa8f8
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-11-01 18:48:55 +0100

    Dodanie plików *.glsl

--- CGA2/Phong_FS.glsl
+++ CGA2/Phong_FS.glsl
@@ -1,5 +1,6 @@
 // Put the names of all group members here 
-//
+// Arkadiusz Gabrys qe83mepi
+// Agnieszka Zacher by57zeja
 
 void main() {
 
--- CGA2/Phong_VS.glsl
+++ CGA2/Phong_VS.glsl
@@ -1,5 +1,6 @@
 // Put the names of all group members here 
-//
+// Arkadiusz Gabrys qe83mepi
+// Agnieszka Zacher by57zeja
 
 uniform vec3 D_LightDir;
 uniform vec3 P_LightPos;
