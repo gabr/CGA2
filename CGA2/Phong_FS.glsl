6c3e05b1a735c63741977d8a3dd3e86b1767b4f4
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-11-02 19:07:33 +0100

    Phong Shading

--- CGA2/Phong_FS.glsl
+++ CGA2/Phong_FS.glsl
@@ -2,6 +2,64 @@
 // Arkadiusz Gabrys qe83mepi
 // Agnieszka Zacher by57zeja
 
+in vec3 lightDNormal;
+in vec3 lightPNormal;
+in vec3 camera;
+
+in vec4 vertexColor;
+varying vec4 vertexPos;
+varying vec3 vertexNormal;
+
 void main() {
 
+    vec3 r;
+    int s = 20;
+    float dotProductTmp = 0;
+    vec3 kSpec = vec3(1.0);
+    vec3 cameraVec = normalize(vertexPos.xyz - camera);
+
+    vec3 lightP_iSpec;
+    vec3 lightP_iDiff;
+    vec3 lightD_iDiff;
+
+    // light D
+    // diff
+    dotProductTmp = dot(vertexNormal, lightDNormal);
+    if (dotProductTmp < 0)
+    {
+        lightD_iDiff = vec3(0.0);
+    }
+    else
+    {
+        lightD_iDiff = vertexColor.xyz * dotProductTmp;
+    }
+
+    // light P
+    // spec
+    r = reflect(lightPNormal, vertexNormal);
+    dotProductTmp = dot(cameraVec, r);
+    if (dotProductTmp < 0.0)
+    {
+        lightP_iSpec = vec3(0.0);
+    }
+    else
+    {
+        lightP_iSpec = kSpec * pow(dotProductTmp, s);
+    }
+
+    // diff    
+    dotProductTmp = dot(vertexNormal, lightPNormal);
+    if (dotProductTmp < 0)
+    {
+        lightP_iDiff = vec3(0.0);
+    }
+    else
+    {
+        lightP_iDiff = vertexColor.xyz * dotProductTmp;
+    }
+
+
+    // result
+    vec3 result = lightP_iSpec + lightP_iDiff + lightD_iDiff;
+    gl_FragColor = vec4(result.x, result.y, result.z, 1.0);
 }
--- CGA2/Phong_VS.glsl
+++ CGA2/Phong_VS.glsl
@@ -6,7 +6,24 @@ uniform vec3 D_LightDir;
 uniform vec3 P_LightPos;
 uniform vec3 cameraPos;
 
+out vec3 lightDNormal;
+out vec3 lightPNormal;
+out vec3 camera;
+
+out vec4 vertexColor;
+varying vec4 vertexPos;
+varying vec3 vertexNormal;
 
 void main() {
 
+    vertexPos = gl_Vertex;
+    vertexNormal = gl_Normal;
+    vertexColor = gl_Color;
+
+    camera = cameraPos;
+
+    lightDNormal = normalize(-D_LightDir);
+    lightPNormal = -normalize(gl_Vertex.xyz - P_LightPos);
+
+    gl_Position = ftransform();
 }
